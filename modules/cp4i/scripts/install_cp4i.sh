fabaabbb559ecb40c33a9ca2c5ada072f9e13190
Kok Sing Khong <khongks@gmail.com>
2021-11-08 16:09:58 +1100

    Update cp4i

--- modules/cp4i/scripts/install_cp4i.sh
+++ modules/cp4i/scripts/install_cp4i.sh
@@ -92,7 +92,3 @@ done
 route=$(oc get route -n ${NAMESPACE} cp4i-navigator-pn -o json | jq -r .spec.host)
 pass=$(oc get secret -n ibm-common-services platform-auth-idp-credentials -o json | jq -r '.data.admin_password' | base64 -d)
 user=$(oc get secret -n ibm-common-services platform-auth-idp-credentials -o json | jq -r '.data.admin_username' | base64 -d)
-
-echo $route
-echo $pass
-echo $user
