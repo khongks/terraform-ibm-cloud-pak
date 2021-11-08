bafa60ae62d435a1d9082949a39adbb19c77e4b1
Kok Sing Khong <khongks@gmail.com>
2021-11-08 15:19:46 +1100

    Update cp4i

--- modules/cp4i/scripts/get_endpoints.sh
+++ modules/cp4i/scripts/get_endpoints.sh
@@ -29,8 +29,8 @@ route=$(oc get route -n ${NAMESPACE} cp4i-navigator-pn -o json | jq -r .spec.hos
 pass=$(oc get secret -n ibm-common-services platform-auth-idp-credentials -o json | jq -r '.data.admin_password' | base64 -d)
 user=$(oc get secret -n ibm-common-services platform-auth-idp-credentials -o json | jq -r '.data.admin_username' | base64 -d)
 
-echo $route
-echo $pass
-echo $user
+echo ${route}
+echo ${pass}
+echo ${user}
 
 # results "${route}" "${pass}" "${user}"
