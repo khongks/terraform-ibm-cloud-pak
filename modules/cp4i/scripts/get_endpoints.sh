a150884d785f18e07ec3def874952d06d5d61663
Kok Sing Khong <khongks@gmail.com>
2021-11-08 15:58:35 +1100

    Update cp4i

--- modules/cp4i/scripts/get_endpoints.sh
+++ modules/cp4i/scripts/get_endpoints.sh
@@ -6,23 +6,23 @@ NAMESPACE=${NAMESPACE:-cp4i}
 eval "$(jq -r '@sh "export KUBECONFIG=\(.kubeconfig) NAMESPACE=\(.namespace)"')"
 
 # Obtains the credentials and endpoints for the installed CP4I Dashboard
-# results() {
-#   console_url_address=$1
-#   password=$2
-#   username=$3
+results() {
+  console_url_address=$1
+  password=$2
+  username=$3
 
-#   # NOTE: The credentials are static and defined by the installer, in the future this
-#   # may not be the case.
-#   # username="admin"
+  # NOTE: The credentials are static and defined by the installer, in the future this
+  # may not be the case.
+  # username="admin"
 
-#   jq -n \
-#     --arg endpoint "$console_url_address" \
-#     --arg username "$username" \
-#     --arg password "$password" \
-#     '{ "endpoint": $endpoint, "username": $username, "password": $password }'
+  jq -n \
+    --arg endpoint "$console_url_address" \
+    --arg username "$username" \
+    --arg password "$password" \
+    '{ "endpoint": $endpoint, "username": $username, "password": $password }'
 
-#   exit 0
-# }
+  exit 0
+}
 
 # POD=$(oc get pods -n cpd-meta-ops | grep ibm-cp-data-operator | awk '{print $1}')
 # control_plane_log=$(oc logs -n cpd-meta-ops $POD | sed 's/[[:cntrl:]]\[[0-9;]*m//g' | tail -20)
@@ -32,4 +32,5 @@ route=$(oc get route -n ${NAMESPACE} cp4i-navigator-pn -o json | jq -r .spec.hos
 pass=$(oc get secret -n ibm-common-services platform-auth-idp-credentials -o json | jq -r '.data.admin_password' | base64 -d)
 user=$(oc get secret -n ibm-common-services platform-auth-idp-credentials -o json | jq -r '.data.admin_username' | base64 -d)
 
-# results "${route}" "${pass}" "${user}"
+echo $route
+results "${route}" "${pass}" "${user}"
