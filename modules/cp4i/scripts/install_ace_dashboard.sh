75c93a03b380b2e996a6623b5294e5fbe596332e
Kok Sing Khong <khongks@gmail.com>
2021-11-08 23:56:16 +1100

    Fixed errors

--- modules/cp4i/scripts/install_ace_dashboard.sh
+++ modules/cp4i/scripts/install_ace_dashboard.sh
@@ -27,7 +27,7 @@ create_secret() {
   namespace=$2
   link=$3
 
-  found=(oc get secret ${secret_name} -n ${NAMESPACE} --ignore-not-found -ojson | jq -r .metadata.name)
+  found=$(oc get secret ${secret_name} -n ${NAMESPACE} --ignore-not-found -ojson | jq -r .metadata.name)
   if [[ ${found} != ${secret_name} ]]; then
     echo "Creating secret ${secret_name} on ${NAMESPACE} from entitlement key"
     oc get secret ibm-entitlement-key -n ${NAMESPACE} --ignore-not-found
